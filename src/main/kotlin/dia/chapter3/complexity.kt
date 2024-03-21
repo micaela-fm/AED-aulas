30d8389f3c00d1846081f006a3b153cd558d0908
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2024-03-21 12:14:53 +0000

    refactored structure & added chapter 6

---
+++ src/main/kotlin/dia/chapter3/complexity.kt
@@ -0,0 +1,14 @@
+package dia.chapter3
+
+fun eliminaEspacos(str: String): String {
+    val n = str.length
+    var nova = ""
+
+    for (i in 0..< n) {
+        if (str[i] != ' ') {
+            nova = nova + str[i]
+        }
+    }
+    return nova
+}
+
---
+++ src/main/kotlin/dia/chapter3/factorial.kt
@@ -0,0 +1,7 @@
+package dia.chapter3
+
+fun factorial(n: Int): Int {
+    return if(n == 0) 1
+    else n * factorial(n-1)
+}
+
