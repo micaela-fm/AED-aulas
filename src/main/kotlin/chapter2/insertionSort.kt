5cb20932e7d5653b9f38f670b6d1880d4a988a43
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2024-03-29 20:23:17 +0000

    added documentation for chapter 2

--- src/main/kotlin/chapter2/insertionSort.kt
+++ src/main/kotlin/chapter2/insertionSort.kt
@@ -1,15 +1,26 @@
 package chapter2
 
-// Complexidade temporal - O(n^2)
-// Complexidade espacial . O(1)
+/**
+ * This function sorts an array using the Insertion Sort algorithm.
+ *
+ * Insertion Sort is a simple sorting algorithm that builds the final sorted array one item at a time.
+ * It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+ *
+ * @param a The array of integers to be sorted.
+ * @param l The left index from where to start the sorting.
+ * @param r The right index where to end the sorting.
+ *
+ * Time complexity - O(n^2)
+ * Space complexity - O(1)
+ */
 fun insertionSort(a: IntArray, l: Int, r: Int) {
-    for (i in l+1..r) {
-        val v = a[i]        // v guarda o elemento a comparar
+    for (i in l + 1..r) {
+        val v = a[i]        // v holds the element to compare
         var j = i
-        while (j > l && v < a[j-1]) {
-            a[j] = a[j-1]   // coloca um elemento do array ordenado na posição do elemento a comparar
+        while (j > l && v < a[j - 1]) {
+            a[j] = a[j - 1]   // places an element of the sorted array in the position of the element to compare
             j--
         }
-        a[j] = v // coloca o elemento a comparar na posição do elemento do array ordenado
+        a[j] = v // places the element to compare in the position of the element of the sorted array
     }
 }
--- src/main/kotlin/chapter2/selectionSort.kt
+++ src/main/kotlin/chapter2/selectionSort.kt
@@ -1,23 +1,49 @@
 package chapter2
 
-// versão IntArray
+/**
+ * This function exchanges the elements at two positions in an integer array.
+ *
+ * @param a The array in which the elements are to be exchanged.
+ * @param i The index of the first element.
+ * @param j The index of the second element.
+ */
 fun exchange(a: IntArray, i: Int, j: Int) {
     val temp = a[i]
     a[i] = a[j]
     a[j] = temp
 }
 
-// versão Array<Int>
+/**
+ * This function exchanges the elements at two positions in an array of any type.
+ *
+ * @param a The array in which the elements are to be exchanged.
+ * @param i The index of the first element.
+ * @param j The index of the second element.
+ */
 fun <T> exchange(a: Array<T>, i: Int, j: Int) {
     val temp = a[i]
     a[i] = a[j]
     a[j] = temp
 }
 
+/**
+ * This function sorts an array using the Selection Sort algorithm.
+ *
+ * Selection Sort is a simple sorting algorithm that maintains a sorted subarray and an unsorted subarray in a given array.
+ * The smallest element is selected from the unsorted array and swapped with the leftmost element, and that element becomes a part of the sorted array.
+ * This process continues moving unsorted array boundary by one element to the right.
+ *
+ * @param a The array of integers to be sorted.
+ * @param left The left index from where to start the sorting.
+ * @param right The right index where to end the sorting.
+ *
+ * Time complexity - O(n^2)
+ * Space complexity - O(1)
+ */
 fun selectionSort(a: IntArray, left: Int, right: Int) {
     for (i in left until right) {
         var min = i
-        for (j in i+1..right) {
+        for (j in i + 1..right) {
             if (a[min] > a[j]) min = j
         }
         exchange(a, min, i)
